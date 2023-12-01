eef9828cfed82ac361ad61eae4e73a56fc698e78
LordofsithSL2 <125189788+LordofsithSL2@users.noreply.github.com>
2023-12-01 16:33:58 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -2,11 +2,22 @@ function skillsMember()
 {
     var member = document.getElementById("member");
     var skills = document.getElementById("skills");
-    var projects = document.getElementById("projects");
     var contact = document.getElementById("contact");
-
+    var about = document.getElementById("about");
+    var projects = document.getElementById("projects");
+    var memberButton = document.getElementById("memberButton");
+    var skillsButton = document.getElementById("skillsButton");
+    var contactButton = document.getElementById("contactButton");
+    var aboutButton = document.getElementById("aboutButton");
+    var projectsButton = document.getElementById("projectsButton");
     member.style.display = "block";
     skills.style.display = "none";
-    projects.style.display = "none";
     contact.style.display = "none";
+    about.style.display = "none";
+    projects.style.display = "none";
+    memberButton.style.backgroundColor = "#333";
+    skillsButton.style.backgroundColor = "#111";
+    contactButton.style.backgroundColor = "#111";
+    aboutButton.style.backgroundColor = "#111";
+    projectsButton.style.backgroundColor = "#111";
 }
